d4115271c71c955ecb940003e75410ba42f3390b
hungphong0910 <cityzen0910@gmail.com>
2020-08-27 10:11:33 +0700

    Chapter 8: Basic login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
+require('jquery');
+import 'bootstrap'
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
